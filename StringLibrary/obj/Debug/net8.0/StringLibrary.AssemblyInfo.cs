ead778333b61f2368c86068f8c7610109924a3e5
Maria Nav <mnavar17@my.centennialcollege.ca>
2024-04-09 19:16:57 -0400

    Console app

--- StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
+++ StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0480b19dd3c309a3272c9cbe93588284b982dda2")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
